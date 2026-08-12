69efcd8ddf0e7cdd2eaf91c3ad784479c4b055a3
Nicolas David Cubillos <nicolasdavidcubillos@gmail.com>
2026-08-12 16:42:39 -0500

    Elimina estructura Java antigua duplicada

--- Java/13. Excepciones/src/main/java/com/puj/EdadInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package com.puj;
-
-public class EdadInvalidaException extends Exception {
-    public EdadInvalidaException() {
-        super("Ocurrio un error, la edad es invalida.");
-    }
-}
--- Java/8. Enums/src/main/java/enums/EnumPrueba.java
+++
@@ -1,7 +0,0 @@
-package enums;
-
-public enum EnumPrueba {
-    BANCO_DE_BOGOTA,
-    VALOR2,
-    VALOR3
-}
